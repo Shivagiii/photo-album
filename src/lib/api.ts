de8e447558d3ad9f2ddea571da67658fe1ebf956
Shivagiii <75017771+Shivagiii@users.noreply.github.com>
2026-04-21 12:32:54 +0000

    added - api.buildyourown.co.in

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ import {
 } from "@/app/types";
 import axios from "axios";
 
-export const API_BASE = "http://127.0.0.1:8000";
+export const API_BASE = "https://api.buildyourown.co.in";
 
 export const fetchbACKEND = async () => {
   try {
